7047b1a5fd8d0ff0d47c87ebe96a8a2a20997ce8
Kong Kunru <apexbugfinder@gmail.com>
2022-06-30 00:00:52 -0700

    everything working on new server -fixed connection problem

--- Portfolio/Portfolio.WebApp/Helpers/GetConnConstants.cs
+++ Portfolio/Portfolio.WebApp/Helpers/GetConnConstants.cs
@@ -4,7 +4,7 @@ namespace Portfolio.WebApp.Helpers {
   public static class GetConnConstants {
 
     private static IConfiguration Configuration {get; set;}
-    public static string PortfolioDB = "Server=198.211.29.93,1433; Database = PortfolioDB;User Id = sa;Password = 'Apple&Pie79';MultipleActiveResultSets=true;Persist Security Info=True;";
+    public static string PortfolioDB = "Server=23.94.40.225,1433; Database = PortfolioDB;User Id = sa;Password = 'Apple&Pie79';MultipleActiveResultSets=true;Persist Security Info=True;";
 
     public static string Authority = "https://identity.portside.sbs";
 
